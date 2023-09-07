97ae78b54bb8fc8791ed5caa874c2bbf5cbc14b5
Sunil H N <sunilh.n27@gmail.com>
2023-09-08 01:23:30 +0530

    added animation

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,24 @@ import Contact from '@/Components/Contact'
 import Features from '@/Components/Features'
 import Hero from '@/Components/Hero'
 import Services from '@/Components/Services'
+import AnimationEffect from './Animation'
 
 
 export default function Home() {
   return (
     <main>
-      <Hero />
-      <Features />
-      <Services />
-      <Contact/>
+      <AnimationEffect delay={0.3}>
+        <Hero />
+      </AnimationEffect>
+      <AnimationEffect delay={0.3}>
+        <Features />
+      </AnimationEffect>
+      <AnimationEffect delay={0.3}>
+        <Services />
+      </AnimationEffect>
+      <AnimationEffect delay={0.3}>
+        <Contact />
+      </AnimationEffect>
     </main>
   )
 }
